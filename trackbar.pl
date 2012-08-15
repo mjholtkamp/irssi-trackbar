fe939faf61500dd5a706be4c084faf8ec33b3c6a
Michiel Holtkamp <git@elfstone.nl>
2012-08-15 18:04:25 +0900

    Completed license information

--- trackbar.pl
+++ trackbar.pl
@@ -1,4 +1,22 @@
 # trackbar.pl
+#
+# Track what you read last when switching to a window.
+#
+#    Copyright (C) 2003,2012  Peter Leurs
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # 
 # This little script will do just one thing: it will draw a line each time you
 # switch away from a window. This way, you always know just upto where you've
